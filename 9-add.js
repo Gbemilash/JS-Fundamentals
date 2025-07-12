f23bc369c1530627c6a9ebb527cc9017c401c648
Gbemisola <lasekangbemi@gmail.com>
2025-07-12 19:52:37 +0000

    Add 9-add.js to sum two integers with add function

--- 9-add.js
+++ 9-add.js
@@ -4,17 +4,7 @@ function add(a, b) {
   return a + b;
 }
 
-if (args[0] === undefined) {
-  console.log("No argument");
-} else if (args[1] === undefined) {
-  console.log("Argument: " + args[0]);
-} else {
-  const num1 = parseInt(args[0], 10);
-  const num2 = parseInt(args[1], 10);
+const num1 = parseInt(args[0], 10);
+const num2 = parseInt(args[1], 10);
 
-  if (isNaN(num1) || isNaN(num2)) {
-    console.log("Please provide valid integers");
-  } else {
-    console.log(add(num1, num2));
-  }
-}
+console.log(add(num1, num2));
